298e2ca416fbe5f172760f05ecd769ace1ddb419
m11dedhia <m11dedhia@gmail.com>
2024-03-07 20:29:49 -0500

    completed q1 part 1

--- hw3/q1.py
+++ hw3/q1.py
@@ -1,5 +1,4 @@
 import libvirt
-import sys
 import random
 import time
 from xml.dom import minidom
@@ -16,15 +15,17 @@ def getMacAddresses(dom, guest_name):
 
     macs = {}
     interfaceTypes = xml.getElementsByTagName("interface")
+    interface_type = {}
     for interfaceType in interfaceTypes:
         # print("interface: type=" + interfaceType.getAttribute("type"))
         interfaceNodes = interfaceType.childNodes
         # print(interfaceNodes[0].nodeName)
         mac = ''
-        interface_type = {}
         for interfaceNode in interfaceNodes:
             if interfaceNode.nodeName == "source":
                 # print(interfaceNode.attributes[interfaceType.getAttribute("type")].value)
+                # print(interfaceType.getAttribute("type"))
+                # print(interface_type)
                 # if interfaceNode.attributes[interfaceType.getAttribute("type")].value == 'default':
                 macs[interfaceNode.attributes[interfaceType.getAttribute("type")].value + '-' + guest_name] = mac
                 interface_type[interfaceNode.attributes[interfaceType.getAttribute("type")].value + '-' + guest_name] = interfaceType.getAttribute("type")
@@ -35,36 +36,51 @@ def getMacAddresses(dom, guest_name):
                     if interfaceNode.attributes[attr].name == "address":
                         # print(networkName)
                         mac = interfaceNode.attributes[attr].value
+    # print(interface_type)
     return macs, interface_type
 
 mac_addresses = {}
 interface_types = {}
 list = conn.listAllDomains()
 xmls = {}
+xmls_string = {}
 for domain in list:
     dom = conn.lookupByID(domain.ID())
     # raw_xml = dom.XMLDesc()
     mac_addresses[domain.name()], types = getMacAddresses(dom, domain.name())
     xmls[domain.name()] = domain
+    xmls_string[domain.name()] = xmls[domain.name()].XMLDesc()
+    # print(types)
     interface_types.update(types)
+    # print(interface_types)
     # print(domain.name(), domain.interfaceAddresses(2))
     # print(dom.XMLDesc())
+# print(interface_types)
+print('MAC addresses of all guest VMs:')
+for guest_vm in mac_addresses:
+    print(guest_vm + ':')
+    for net_interface in mac_addresses[guest_vm]:
+        print('\t' + net_interface.split('-')[0] + ':\t' + mac_addresses[guest_vm][net_interface])
+    print('\n')
+print('\n')
 
 mac_list = {}
+to_restart = {}
 for vm in mac_addresses:
     # print(dir(xmls[vm]))
     for net_interface in mac_addresses[vm]:
-        print(mac_list)
+        # print(mac_list)
         if mac_addresses[vm][net_interface] not in mac_list:
             # if mac == 'default':
             # mac = mac + '-' + vm
             mac_list[mac_addresses[vm][net_interface]] = net_interface
         else:
             # trigger mac update function
-            xml = ET.fromstring(xmls[vm].XMLDesc())
-            # print(ET.tostring(xml, encoding='unicode'))
+            # xml = ET.fromstring(xmls[vm].XMLDesc()) if isinstance(xmls[vm], libvirt.virDomain) else ET.fromstring(xmls[vm])
+            print('Duplicate MAC Address found at ' + net_interface)
+            xml = ET.fromstring(xmls_string[vm])
             finder = './devices/interface[@type=\'network\']' if (interface_types[net_interface] == 'network') else './devices/interface[@type=\'bridge\']'
-            print(finder)
+            # print(finder)
             mac = xml.find(finder + '/mac')
             interface = xml.find(finder)
             # print(ET.tostring(interface, encoding='unicode'))
@@ -72,13 +88,14 @@ for vm in mac_addresses:
             # interface = xml.find('./devices/interface[@type=\'bridge\']')
             # print(ET.tostring(interface, encoding='unicode'))
             # xmls[vm].updateDeviceFlags(ET.tostring(interface, encoding='unicode'))
+            xmls_string[vm] = ET.tostring(xml, encoding='unicode')
             conn.defineXMLFlags(ET.tostring(xml, encoding='unicode'))
             # print(dir(conn))
-            xmls[vm].shutdown()
-            time.sleep(10)
-            xmls[vm].create()
+            to_restart[vm] = True
+# print(to_restart)
+for vm in to_restart:
+    xmls[vm].shutdown()
+    time.sleep(10)
+    xmls[vm].create()
 
 conn.close()
-
-
-print(mac_addresses)
